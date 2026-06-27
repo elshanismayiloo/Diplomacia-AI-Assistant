cd13a9f637fdba41c3947f1eb6d65d0b1e764ea6
Elshan <78827424+elshanismayiloo@users.noreply.github.com>
2026-06-28 01:40:37 +0400

    Implement complete RAG pipeline

--- backend/test_chat.py
+++ backend/test_chat.py
@@ -1,27 +1,20 @@
-from pathlib import Path
-
-from backend.chat import ask_ai
-from backend.search import search_knowledge
+from backend.chat import ask
 
 
-SYSTEM_PROMPT = Path(
-    "prompts/system_prompt.md"
-).read_text(
-    encoding="utf-8"
-)
+while True:
 
-QUESTION = input("Question: ")
+    question = input("\nYou: ")
 
-CONTEXT = search_knowledge(
-    QUESTION
-)
+    if question.lower() in [
+        "exit",
+        "quit"
+    ]:
+        break
 
-print()
+    answer = ask(question)
 
-print(
-    ask_ai(
-        SYSTEM_PROMPT,
-        CONTEXT,
-        QUESTION
-    )
-)
+    print()
+
+    print("AI:")
+
+    print(answer)
